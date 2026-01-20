b449cfccf471bb46694c83d637c85f9880332357
Taj Mace <taj.mac101@gmail.com>
2026-01-20 15:26:05 +1100

    initial setup

---
+++ ruby-mobile/src/services/api.ts
@@ -0,0 +1,12 @@
+/**
+ * =============<< ********* >>=============
+ * Author       : Taj Macé
+ * Email        : taj.mac101@gmail.com
+ * Organization : Taj Macé
+ * Created On   : 2026-01-20
+ * Title        : api.ts
+ * Description  : axios file to link the backend api to the frontend through api calls
+ * Copyright (c) 2025 Taj Macé.
+ * =============<< ********* >>=============
+ */
+
