9e1e7f234623780e4e2c167bf616bbcc1232feec
Taj Mace <taj.mac101@gmail.com>
2026-01-22 20:16:07 +1100

    included file to deal with front end calendar logic

---
+++ ruby-mobile/src/utils/calendarUtils.ts
@@ -0,0 +1,11 @@
+/**
+ * =============<< ********* >>=============
+ * Author       : Taj Macé
+ * Email        : taj.mac101@gmail.com
+ * Organization : Taj Macé
+ * Created On   : 2026-01-22
+ * Title        : calendarUtils.ts
+ * Description  : Utility functions for calendar view logic
+ * Copyright (c) 2025 Taj Macé.
+ * =============<< ********* >>=============
+ */
