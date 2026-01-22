8e0a7412aae6e15ac803beca1e82f70735934b70
Taj Mace <taj.mac101@gmail.com>
2026-01-22 19:59:28 +1100

    added colour palette for project with semantic naming

---
+++ ruby-mobile/src/theme/colours.ts
@@ -0,0 +1,19 @@
+/**
+ * =============<< ********* >>=============
+ * Author       : Taj Macé
+ * Email        : taj.mac101@gmail.com
+ * Organization : Taj Macé
+ * Created On   : 2026-01-22
+ * Title        : colours.ts
+ * Description  : colour choices across the project
+ * Copyright (c) 2025 Taj Macé.
+ * =============<< ********* >>=============
+ */
+
+export const colours = {
+    primary: "#A91D3A",     // Carmine
+    secondary: "#BFC3BA",   // Ash Grey
+    tertiary: "#A9ACA9",    // Silver
+    dark: "#210F04",        // Coffee Bean
+    accent: "#623CEA"       // Majorelle Blue     
+};
