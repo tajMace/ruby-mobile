a91cb5f88699200699fb49ebcb267d45b4e5a863
Taj Mace <taj.mac101@gmail.com>
2026-01-22 19:43:35 +1100

    included an index file in the new theme directory for ease of access

---
+++ ruby-mobile/src/theme/index.ts
@@ -0,0 +1,11 @@
+/**
+ * =============<< ********* >>=============
+ * Author       : Taj Macé
+ * Email        : taj.mac101@gmail.com
+ * Organization : Taj Macé
+ * Created On   : 2026-01-22
+ * Title        : index.ts
+ * Description  : main export for project's aesthetic presets
+ * Copyright (c) 2025 Taj Macé.
+ * =============<< ********* >>=============
+ */
